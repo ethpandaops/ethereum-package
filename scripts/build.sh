906140a95010ae9532e051dc4969c40e536a2970
Gyanendra Mishra <anomaly.the@gmail.com>
2022-10-27 12:34:13 -0300

    stuff works

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,6 +11,6 @@ root_dirpath="$(dirname "${script_dirpath}")"
 #                                             Main Logic
 # ==================================================================================================
 
-if ! bash "kurtosis" "startosis" "exec" "${root_dirpath}"; then
+if ! "kurtosis" "startosis" "exec" "${root_dirpath}"; then
     echo "Error: running the startosis script"
 fi
